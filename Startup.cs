37255d6f14a0a5178fc1a47286e43b8ed4e60458
Brian Coleman <brian@meloyelo.com>
2016-05-25 14:39:39 -0600

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+        }
+        
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Program>()
+                .Build();
+                
+            host.Run();
+        }
+    }
+}
